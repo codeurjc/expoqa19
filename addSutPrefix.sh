a0b54b878bbaa55faba676e14e0f1e9269a4ff0b
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-06-17 12:54:19 +0200

    Modify script to use pwd value

--- addSutPrefix.sh
+++ addSutPrefix.sh
@@ -1,4 +1,5 @@
 #/!bin/bash
 # Add sut prefix for the current execution 
-cd ${PWD}; sed -i -e "s/name: web/name: ${ET_SUT_CONTAINER_NAME}-web/g" -e 's/sut_/sut-/g' web-deployment.yaml
-cd ${PWD}; sed -i -e "s/name: db/name: ${ET_SUT_CONTAINER_NAME}-db/g" -e 's/sut_/sut-/g' db-deployment.yaml
+echo ${PWD}
+cd ${PWD}/k8s; sed -i -e "s/name: web/name: ${ET_SUT_CONTAINER_NAME}-web/g" -e 's/sut_/sut-/g' web-deployment.yaml
+cd ${PWD}/k8s; sed -i -e "s/name: db/name: ${ET_SUT_CONTAINER_NAME}-db/g" -e 's/sut_/sut-/g' db-deployment.yaml
