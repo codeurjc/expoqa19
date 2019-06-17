b6137ed235202b79fbe7365c9a99600a6c557916
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-06-17 11:36:57 +0200

    Refactor. Remove webapp folders

---
+++ addSutPrefix.sh
@@ -0,0 +1,4 @@
+#/!bin/bash
+# Add sut prefix for the current execution 
+sed -i -e 's/name: web/name: ${env.ET_SUT_CONTAINER_NAME}-web/g' -e 's/sut_/sut-/g' web-deployment.yaml
+sed -i -e 's/name: db/name: ${env.ET_SUT_CONTAINER_NAME}-db/g' -e 's/sut_/sut-/g' db-deployment.yaml
