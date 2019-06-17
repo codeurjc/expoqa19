e99cfbe640399b2b490b04140fa39068262abbd0
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-06-17 12:56:10 +0200

    Fix script

--- addSutPrefix.sh
+++ addSutPrefix.sh
@@ -1,5 +1,5 @@
 #/!bin/bash
 # Add sut prefix for the current execution 
-echo ${PWD}
-cd ${PWD}/k8s; sed -i -e "s/name: web/name: ${ET_SUT_CONTAINER_NAME}-web/g" -e 's/sut_/sut-/g' web-deployment.yaml
-cd ${PWD}/k8s; sed -i -e "s/name: db/name: ${ET_SUT_CONTAINER_NAME}-db/g" -e 's/sut_/sut-/g' db-deployment.yaml
+cd ${PWD}/k8s
+sed -i -e "s/name: web/name: ${ET_SUT_CONTAINER_NAME}-web/g" -e 's/sut_/sut-/g' web-deployment.yaml
+sed -i -e "s/name: db/name: ${ET_SUT_CONTAINER_NAME}-db/g" -e 's/sut_/sut-/g' db-deployment.yaml
