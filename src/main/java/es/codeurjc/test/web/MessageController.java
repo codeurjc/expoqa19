bfff67a69d1d01592ecc6efa17a3ca6dd2817bbb
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-06-17 12:40:20 +0200

    Add fix for the app

--- src/main/java/es/codeurjc/test/web/MessageController.java
+++ src/main/java/es/codeurjc/test/web/MessageController.java
@@ -1,7 +1,5 @@
 package es.codeurjc.test.web;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -11,31 +9,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MessageController {
 
-	private static Logger LOG = LoggerFactory.getLogger(MessageController.class);
+    private static Logger LOG = LoggerFactory
+            .getLogger(MessageController.class);
 
-	@Autowired
-	private MessageRepository repository;
+    @Autowired
+    private MessageRepository repository;
 
-	@GetMapping("/")
-	public String showMessages(Model model) {
+    @GetMapping("/")
+    public String showMessages(Model model) {
 
-		model.addAttribute("messages", repository.findAll());
+        model.addAttribute("messages", repository.findAll());
 
-		return "index";
-	}
+        return "index";
+    }
 
-	@PostMapping("/")
-	public String newMessage(Message message) {
+    @PostMapping("/")
+    public String newMessage(Message message) {
 
-		repository.save(message);
+        repository.save(message);
 
-		LOG.info("Message "+message+" added");
+        LOG.info("Message " + message + " added");
 
-		return "redirect:/";
-	}
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete")
+    public String deleteMessage(@RequestParam String title) {
+        LOG.info("Delete message with title: " + title);
+        try {
+            List<Message> messages = repository.findByTitle(title).get();
+            Message message = messages.get(0);
+            repository.delete(message);
+            LOG.info("Message " + message + " deleted");
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+        }
+        return "redirect:/";
+    }
 
 }
--- src/main/java/es/codeurjc/test/web/MessageRepository.java
+++ src/main/java/es/codeurjc/test/web/MessageRepository.java
@@ -1,7 +1,10 @@
 package es.codeurjc.test.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-
+    public Optional<List<Message>> findByTitle(String title);
 }
