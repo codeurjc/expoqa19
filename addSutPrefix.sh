4125e9c6b3bfccd841e42c791f33f07f5b97e264
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-06-17 11:40:57 +0200

    Modify script

--- addSutPrefix.sh
+++ addSutPrefix.sh
@@ -1,4 +1,4 @@
 #/!bin/bash
 # Add sut prefix for the current execution 
-sed -i -e 's/name: web/name: ${env.ET_SUT_CONTAINER_NAME}-web/g' -e 's/sut_/sut-/g' web-deployment.yaml
-sed -i -e 's/name: db/name: ${env.ET_SUT_CONTAINER_NAME}-db/g' -e 's/sut_/sut-/g' db-deployment.yaml
+cd k8s; sed -i -e 's/name: web/name: ${env.ET_SUT_CONTAINER_NAME}-web/g' -e 's/sut_/sut-/g' web-deployment.yaml
+cd k8s; sed -i -e 's/name: db/name: ${env.ET_SUT_CONTAINER_NAME}-db/g' -e 's/sut_/sut-/g' db-deployment.yaml
