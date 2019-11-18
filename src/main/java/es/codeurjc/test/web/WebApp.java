caf3ba6bf98fe15558d0f495737d452042209e6d
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-11-18 15:57:02 +0100

    revert

--- src/main/java/es/codeurjc/test/web/WebApp.java
+++ src/main/java/es/codeurjc/test/web/WebApp.java
@@ -1,6 +1,5 @@
 package es.codeurjc.test.web;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,28 +7,24 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class WebApp {
 
-	@Value("${mysql.db.host}")
-	String mysqlDbHost;
-
 	private static ConfigurableApplicationContext app;
-
-	public static void main(String[] args) throws Exception {
-		System.out.println("MySql host: " + mysqlDbHost);
-		SpringApplication.run(WebApp.class, args);
-	}
-
-	public static void start() {
-		start(new String[] {});
-	}
+	
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApp.class, args);
+    }
+    
+    public static void start() {
+    	start(new String[] {});
+    }
 
 	private static void start(String[] args) {
-		if (app == null) {
-			app = SpringApplication.run(WebApp.class, args);
-		}
-	}
-
+		if(app == null) {
+    		app = SpringApplication.run(WebApp.class, args);
+    	} 
+	}    
+	
 	public static void stop() {
-		if (app != null) {
+		if(app != null) {
 			app.close();
 		}
 	}
